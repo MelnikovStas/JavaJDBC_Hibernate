2aa38873b74d914827d3dce3fca2fab7edcc7e3c
Melnikov Stas <stas1994melnikov@gmail.com>
2025-01-23 17:00:31 +0300

    Util class and DB connection is god!

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соединения с БД
-    final String url = "jdbc:mysql://localhost:3306/";
-    final String user = "root";
-    final String password = "root";
-    public Connection getConnection() {
+    static final String url = "jdbc:mysql://localhost:3306/";
+    static final String user = "root";
+    static final String password = "root";
+    public static Connection getConnection() {
         try ( Connection connection = DriverManager.getConnection(url,user,password)) {
             return connection;
         } catch (SQLException e) {
